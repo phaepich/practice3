e7f175a789a8f515b631e9050a9a27929d25d407
phaepich <sasha-popov04@list.ru>
2023-06-01 20:31:40 +0300

    Pathfinding fix

--- Assets/Scripts/ZombieAI.cs
+++ Assets/Scripts/ZombieAI.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using Pathfinding;
 
-
 public class ZombieAI : MonoBehaviour
 {
     [SerializeField] private float _minWalkableDistance;
@@ -12,15 +11,15 @@ public class ZombieAI : MonoBehaviour
     [SerializeField] private float _targetFollowRange;
     [SerializeField] private float _stopTargetFollowingRange;
     [SerializeField] private EnemyAttack _enemyAttack;
-    [SerializeField]  private AIDestinationSetter _aiDestinationSetter;
+    [SerializeField] private AIDestinationSetter _aiDestinationSetter;
     [SerializeField] private AIPath _aiPath;
     [SerializeField] private float _microphoneDistance;
     public AudioSource source;
     private Animator _animator;
 
     public float threshold = 0.1f;
-
     public float loudnessSensibility = 100;
+
     private Player _player;
     private EnemyStates _currentState;
     private Vector3 _roamPosition;
@@ -45,10 +44,25 @@ public class ZombieAI : MonoBehaviour
             _currentState = EnemyStates.MovingToSound;
         }
     }
+
     Vector3 GenerateRoamPosition()
     {
-        var roamPosition = gameObject.transform.position + GenerateRandomDirection() * GenerateRandomWalkableDistance();
-        return roamPosition;
+        Vector3 randomDirection = GenerateRandomDirection();
+        Vector3 targetPosition = transform.position + randomDirection * GenerateRandomWalkableDistance();
+
+        // Проверить доступность точки перемещения на сетке Grid Graph
+        NNConstraint constraint = NNConstraint.Default;
+        constraint.constrainWalkability = true;
+        constraint.walkable = true;
+        GraphNode node = AstarPath.active.GetNearest(targetPosition, constraint).node;
+
+        if (node != null && !node.Walkable)
+        {
+            // Если точка недоступна, выполнить повторную генерацию
+            return GenerateRoamPosition();
+        }
+
+        return (Vector3)node.position;
     }
 
     float GenerateRandomWalkableDistance()
@@ -62,6 +76,7 @@ public class ZombieAI : MonoBehaviour
         var newDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
         return newDirection.normalized;
     }
+
     void Start()
     {
         _animator = gameObject.GetComponent<Animator>();
@@ -70,6 +85,7 @@ public class ZombieAI : MonoBehaviour
         _currentState = EnemyStates.Roaming;
         _roamPosition = GenerateRoamPosition();
     }
+
     void Update()
     {
         switch (_currentState)
@@ -88,6 +104,7 @@ public class ZombieAI : MonoBehaviour
                 TryFindTarget();
                 TryHearTarget();
                 break;
+
             case EnemyStates.Following:
                 _animator.SetBool("IsFollowing", true);
                 _animator.SetBool("IsWalking", false);
@@ -109,13 +126,14 @@ public class ZombieAI : MonoBehaviour
                     _currentState = EnemyStates.Roaming;
                 }
                 break;
+
             case EnemyStates.MovingToSound:
                 _animator.SetBool("IsFollowing", true);
                 _animator.SetBool("IsWalking", false);
                 _aiPath.maxSpeed = 3;
                 TryHearTarget();
                 _aiDestinationSetter.target = _soundTarget.transform;
-                
+
                 if (Vector3.Distance(gameObject.transform.position, _player.transform.position) < _targetFollowRange)
                 {
                     _currentState = EnemyStates.Following;
