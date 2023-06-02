48db06359c1d44cfa79bbb6f0c5e9b5fbf8a3a10
phaepich <sasha-popov04@list.ru>
2023-06-03 00:50:51 +0300

    umer menu

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -6,12 +7,23 @@ public class Player : MonoBehaviour
     [SerializeField] private int maxHealth;
     [SerializeField] private float healPerSecond;
     public float health;
+    [SerializeField] private Canvas deadCanvas;
+    [SerializeField] private Transform vrCamera;
+
+    private void Start()
+    {
+        deadCanvas.enabled = false;
+        
+    }
 
     public void TakeDamage(int damage)
     {
         if (health - damage <= 0)
         {
             Debug.Log("Умер");
+            deadCanvas.enabled = true;
+            deadCanvas.transform.position = vrCamera.position + vrCamera.forward * 1;
+            deadCanvas.transform.rotation = Quaternion.LookRotation(vrCamera.forward);
             return;
         }
         health -= damage;
