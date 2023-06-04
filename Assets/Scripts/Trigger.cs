a18b349c3de4be6e4c3da3be29a420bb0e2d831b
neverelz <belyavtseva04@mail.ru>
2023-06-05 00:36:04 +0300

    update

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,23 +1,96 @@
+using System;
 using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
+    public AudioClip musicClip;
+    private AudioSource musicSource;
     [SerializeField] private GameObject _gameObject;
+    private bool k = false;
+    public Transform objectToMove; // Объект, который нужно переместить
+    public Vector3 targetPosition; // Целевая позиция, куда нужно переместить объект
+    public bool conditionMet; // Условие, которое должно быть выполнено для начала движения объекта
+    public float moveSpeed = 5f;
+    private bool playerEnteredTrigger; // Флаг, указывающий, что игрок находится в триггере
+    
+    void Start () 
+    {
+        musicSource = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider col)
     {
         if (col.CompareTag("Player"))
         {
             if (gameObject.name == "Trigger1")
             {
-                _gameObject.transform.position = new Vector3(3.17f, 0, 12.74f);
-                _gameObject.transform.rotation = new Quaternion(-0.300304115f,0.915402412f,0.0518756397f,-0.262992233f);
+                _gameObject.transform.position = new Vector3(506.01f, 7.672f , 500.647f);
+                _gameObject.transform.rotation = new Quaternion(0.0506434217f,0.992991865f,-0.00543892104f,-0.106643818f);
+                if (!musicSource.isPlaying)
+                {
+                    musicSource.clip = musicClip;
+                    musicSource.Play();
+                }
             }
-
             if (gameObject.name == "Trigger2")
             {
-                _gameObject.transform.position = new Vector3(12.69f,0,3.07f);
-                _gameObject.transform.rotation = new Quaternion(0.282406986f,0.857821345f,-0.160994112f,-0.398082495f);
+                _gameObject.transform.position = new Vector3(521.974f, 5.174f , 501.793f);
+                _gameObject.transform.rotation = new Quaternion(0,0,0,1);
+                if (!musicSource.isPlaying)
+                {
+                    musicSource.clip = musicClip;
+                    musicSource.Play();
+                }
+            }
+
+            if (gameObject.name == "Trigger3")
+            {
+                playerEnteredTrigger = true;
+                _gameObject.transform.position = new Vector3(526.155f, 13.22f , 512.128f);
+                _gameObject.transform.rotation = new Quaternion(-0.0470253006f,0.994655788f,-0.0166228134f,-0.0904000923f);
+                if (!musicSource.isPlaying)
+                {
+                    musicSource.clip = musicClip;
+                    musicSource.Play();
+                }
+                
             }
         }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (gameObject.name == "Trigger1")
+            {
+                _gameObject.transform.position = new Vector3(0, 0, 0);
+                _gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+                Destroy(gameObject);
+            }
+            if (gameObject.name == "Trigger2")
+            {
+                _gameObject.transform.position = new Vector3(0, 0, 0);
+                _gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+                Destroy(gameObject);
+            }
+            if (gameObject.name == "Trigger3")
+            {
+                playerEnteredTrigger = false;
+                _gameObject.transform.position = new Vector3(0, 0, 0);
+                _gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+    private void Update()
+    {
+        if (playerEnteredTrigger && conditionMet)
+        {
+            MoveObject();
+        }
+    }
+    private void MoveObject()
+    {
+        objectToMove.position = Vector3.MoveTowards(objectToMove.position, targetPosition, Time.deltaTime * moveSpeed);
     }
 }
